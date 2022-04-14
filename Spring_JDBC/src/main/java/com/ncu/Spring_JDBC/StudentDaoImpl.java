ab3c86e31014358746b12979114d2664ff3dd8ec
Nitin Sharma <80587065+NitinS87@users.noreply.github.com>
2022-04-14 10:41:36 +0530

    Update StudentDaoImpl.java

--- Spring_JDBC/src/main/java/com/ncu/Spring_JDBC/StudentDaoImpl.java
+++ Spring_JDBC/src/main/java/com/ncu/Spring_JDBC/StudentDaoImpl.java
@@ -26,6 +26,57 @@ public class StudentDaoImpl implements StudentDAO {
 		
 
 	}
+	
+	@Override
+	public void batchInsertRecords(List<Student> students) {
+		int insertedRows = 0;
+		String query = "insert into student values(?,?,?)";
+		for (Student student : students) {
+			
+			Object[] args = {student.getId(), student.getName(), student.getAddress()};
+			int row = jdbcTemplate.update(query, args);
+			if(row==1) {
+				System.out.println("Record added successfully");
+				insertedRows++;
+			}else {
+				System.out.println("Insertion Failed!");
+			}
+		}
+		
+		if(insertedRows==students.size()) {
+			System.out.println("All records inserted successfully");
+		}
+		else {
+			System.out.println("Insertion failed  m  ");
+		}
+		
+	};
+
+	@Override
+	public void batchUpdateRecords(List<Student> students) {
+		// TODO Auto-generated method stub
+		int insertedRows = 0;
+		String query = "update student set student_name= ?, student_address= ? where student_id= ? ";
+		for (Student student : students) {
+			
+			Object[] args = {student.getName(),student.getAddress(),student.getId()};
+			int row = jdbcTemplate.update(query, args);
+			if(row==1) {
+				System.out.println("Record updated successfully");
+				insertedRows++;
+			}else {
+				System.out.println("Failed to update!");
+			}
+		}
+		
+		if(insertedRows==students.size()) {
+			System.out.println("All records updated successfully");
+		}
+		else {
+			System.out.println("Failed to update");
+		}
+		
+	}
 
 	@Override
 	public List<Student> getAllRecords() {
@@ -41,7 +92,6 @@ public class StudentDaoImpl implements StudentDAO {
 	@Override
 	public void updateRecord(Student student) {
 		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub
 		String query = "update student set student_name= ?, student_address= ? where student_id= ? ";
 		Object[] args = {student.getName(), student.getAddress(), student.getId()};
 		int row = jdbcTemplate.update(query, args);
@@ -50,6 +100,7 @@ public class StudentDaoImpl implements StudentDAO {
 		}else {
 			System.out.println("Updation Failed!");
 		}
+		
 
 	}
 
@@ -57,8 +108,7 @@ public class StudentDaoImpl implements StudentDAO {
 	public void deleteRecord(int id) {
 		// TODO Auto-generated method stub
 		String query = "delete from student where student_id= ?";
-		Object[] args = {id};
-		int row = jdbcTemplate.update(query, args);
+		int row = jdbcTemplate.update(query, id);
 		if(row==1) {
 			System.out.println("Record deleted successfully");
 		}else {
@@ -66,5 +116,19 @@ public class StudentDaoImpl implements StudentDAO {
 		}
 
 	}
+	
+	@Override
+	public void deleteAllRecord() {
+		// TODO Auto-generated method stub
+		String query = "delete from student";
+		int row = jdbcTemplate.update(query);
+		if(row==1) {
+			System.out.println("Records deleted successfully");
+		}else {
+			System.out.println("Deletion Failed!");
+		}
+
+	}
+	
 
 }
